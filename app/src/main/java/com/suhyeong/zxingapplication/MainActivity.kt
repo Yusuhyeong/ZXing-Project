e2e917849aa42337fce76536addb62c85a8d7631
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2024-07-24 09:46:56 +0900

    permission check -> CameraActivity

--- app/src/main/java/com/suhyeong/zxingapplication/MainActivity.kt
+++ app/src/main/java/com/suhyeong/zxingapplication/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.suhyeong.zxingapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+import androidx.core.content.ContextCompat
+import com.suhyeong.zxingapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val CAMERA_PERMISSION_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.imgCameraOpen.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        } else {
+            val cameraIntent = Intent(this, CameraActivity::class.java)
+            startActivity(cameraIntent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                val cameraIntent = Intent(this, CameraActivity::class.java)
+                startActivity(cameraIntent)
+            } else {
+                Toast.makeText(this, "rejected permission.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
